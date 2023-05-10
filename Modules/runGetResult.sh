b1cd842dd3cd53ba1754151acbbfda7e9dd184ae
wq-sunshine <75285805+wq-sunshine@users.noreply.github.com>
2023-05-10 14:58:45 +0800

    Update runGetResult.sh

--- Modules/runGetResult.sh
+++ Modules/runGetResult.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /lustre/home/weizhiqiang/2022_04_16_05_17_21
-python get_result.py /lustre/home/weizhiqiang/2022_04_16_05_17_21/result
+cd /lustre/home/XXXXXX/2022_04_16_05_17_21
+python get_result.py /lustre/home/XXXXXX/2022_04_16_05_17_21/result
