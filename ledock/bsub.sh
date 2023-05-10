35b77f1ebf278e64fcacc70d70a55fb7d1e95b72
wq-sunshine <75285805+wq-sunshine@users.noreply.github.com>
2023-05-10 15:00:34 +0800

    Update bsub.sh

--- ledock/bsub.sh
+++ ledock/bsub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-folder="D:/generatedMolecules/DeepFMPO/deep/ledock/Zinc6000"
-output_file="D:/generatedMolecules/DeepFMPO/deep/ledock/ligands1"
+folder="D:/generatedMolecules/xxxxx/deep/ledock/Zinc6000"
+output_file="D:/generatedMolecules/xxxxxx/deep/ledock/ligands1"
 softfiles=$(ls $folder)
 for sfile in ${softfiles}
 do 
