7c1a7410aa25c8b709ff4b942d2363c119d7bf33
wq-sunshine <75285805+wq-sunshine@users.noreply.github.com>
2023-05-10 15:01:15 +0800

    Update dock.sh

--- ledock/dock.sh
+++ ledock/dock.sh
@@ -9,7 +9,7 @@ fi
 cd $dir
 
 # 高研院
-/lustre/home/weizhiqiang/ledock/QMarine/ledock_go $2
+/lustre/home/xxxxxx/ledock/QMarine/ledock_go $2
 
 # 即墨
-# /home/csywz/deep/ledock/ledock_go $2
+# /home/xxxx/deep/ledock/ledock_go $2
