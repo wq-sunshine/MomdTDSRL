270ac0848108d6cc6c5d882eb2e836e104ffceaa
wq-sunshine <75285805+wq-sunshine@users.noreply.github.com>
2023-05-10 15:02:26 +0800

    Update docking.py

--- ledock/docking.py
+++ ledock/docking.py
@@ -7,7 +7,7 @@ Created on Wed Apr 14 16:00:57 2021
 
 
 import os
-path ='D:/generatedMolecules/DeepFMPO/deep/ledock/Mpro/Mpro'
+path ='D:/generatedMolecules/xxxxxx/deep/ledock/Mpro/Mpro'
 def get_filelist(dir):
     Filelist = []
     for home, dirs, files in os.walk(path):
@@ -28,17 +28,7 @@ if __name__ =="__main__":
             #file_data = ""
             p1,f1 = os.path.split(file)
             p2,f2 = os.path.split(p1)
-            #new_str = os.path.join(p1,f2+"_pro.pdb")
-            #new_str = "/home/b519/lyy/lyy2/DeepFMPO/DeepFMPO-master/3.6/ledock/ligand0"
-            #with open(file, "r", encoding="utf-8") as f:
-                #for line in f:
-                    #if old_str == line:
-                        #line = line.replace(old_str,new_str)
-                    #file_data += line
-            #with open(file,"w",encoding="utf-8") as f:
-                #f.write(file_data)
-            #os.system('cd '+p1)
-            #os.system('/home/b519/lyy/lyy2/DeepFMPO/DeepFMPO-master/3.6/ledock/ledock_go '+file)
-            os.system('sh dock.sh D:/generatedMolecules/DeepFMPO/deep/ledock/result '+file+' '+f2)
+                    
+            os.system('sh dock.sh D:/generatedMolecules/xxxxxx/deep/ledock/result '+file+' '+f2)
             print(file)
         
